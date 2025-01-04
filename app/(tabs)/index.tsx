874d42e5ee2bff06951f1943c5ac4ed2d6039052
Kelvin Ochubili <kelvinochubili@gmail.com>
2025-01-04 17:04:32 +0100

    add image picking functionality; update Button and ImageViewer components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,17 +2,33 @@ import { View, StyleSheet } from 'react-native';
 
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
+	const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+	const pickImageAsync = async () => {
+		let result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ['images'],
+			allowsEditing: true,
+			quality: 1,
+		});
+
+		if (!result.canceled) {
+			setSelectedImage(result.assets[0].uri);
+		} else {
+			alert('Image not picked');
+		}
+	};
 	return (
 		<View style={styles.container}>
 			<View style={styles.imageContainer}>
-				<ImageViewer imgSource={PlaceholderImage} />
+				<ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
 			</View>
 			<View style={styles.footerContainer}>
-				<Button label='Choose a photo' theme='primary' />
+				<Button label='Choose a photo' theme='primary' onPress={pickImageAsync} />
 				<Button label='Use this photo' />
 			</View>
 		</View>
